71e5f51b49915a0aa132c9a3b56fa4db76bc1e55
Rahul Goyal <goyalrahul1516@gmail.com>
2025-01-12 22:33:55 +0530

    final before ep2

--- 6502Test/src/main_6502.cpp
+++ 6502Test/src/main_6502.cpp
@@ -1,22 +1,9 @@
-#if 0
-#include "main_6502.h"
+#include <cstdio>
+#include "gtest/gtest.h"
 
-int main()
-{
-    Memory mem;
-    CPU cpu;
-    cpu.Reset( mem );
-    // start - in-line program here
-    mem[0xFFF0] = CPU::INS_JSR;
-    mem[0xFFF1] = 0x42;
-    mem[0xFFF2] = 0x43;
-    mem[0x4342] = CPU::INS_LDA_IM;
-    mem[0x4343] = 0x84;
-    // end - in-line program here
-    cpu.Execute( 8, mem );
-    printf("Hello");
-    return 0;
+// Normal platforms: program entry point is main, argc/argv are initialized.
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from %s\n", __FILE__);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-#endif
-
-
